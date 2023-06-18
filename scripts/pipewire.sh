257dbe1b9ac3f4abf37852906defa06baed87f51
Boria138 <boriabloger@protonmail.com>
2023-06-18 15:32:56 +0600

    Fix pipewire and update nvidia
    Fix pipewire install script and config
    Add nvidia hardware video acceleration
    Delete wine_unity.sh

--- scripts/pipewire.sh
+++ scripts/pipewire.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-arch-chroot /mnt /bin/bash -c "pacman -Sy --needed alsa-lib alsa-utils alsa-lib alsa-firmware alsa-card-profiles alsa-plugins wireplumber pipewirepipewire-pulse pipewire-alsa pipewire-jack"
+arch-chroot /mnt /bin/bash -c "pacman -Sy --needed --noconfirm alsa-lib alsa-utils alsa-firmware alsa-card-profiles alsa-plugins wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack"
 if aplay -L | grep -q ALC887 ; then
   arch-chroot /mnt /bin/bash -c "mkdir -p /etc/pipewire"
   cp -rf ./tweaks/pipewire/* /mnt/etc/pipewire/
--- tweaks/general/profile.d/nvidia.sh
+++ tweaks/general/profile.d/nvidia.sh
@@ -1,7 +1,46 @@
+nvidia_check_arch(){
+    if [[ $(lspci | grep "VGA\|3D" | sed -rn 's/.*(NVIDIA).*/\1/p') ]]; then
+        [[ $(lspci | grep VGA | sed -rn 's/.*(G[0-9]*).*/\1/p') == G[0-9]* ]] && nv_arch=Tesla
+        [[ $(lspci | grep VGA | sed -rn 's/.*(GT[0-9]*).*/\1/p') == GT[0-9]* ]] && nv_arch=Tesla
+        [[ $(lspci | grep VGA | sed -rn 's/.*(MCP[0-9]*).*/\1/p') == MCP[0-9]* ]] && nv_arch=Tesla
+        [[ $(lspci | grep VGA | sed -rn 's/.*(GF[0-9]*).*/\1/p') == GF[0-9]* ]] && nv_arch=Fermi
+        [[ $(lspci | grep VGA | sed -rn 's/.*(GK[0-9]*).*/\1/p') == GK[0-9]* ]] && nv_arch=Kepler
+        [[ $(lspci | grep VGA | sed -rn 's/.*(GM[0-9]*).*/\1/p') == GM[0-9]* ]] && nv_arch=Maxwell
+        [[ $(lspci | grep VGA | sed -rn 's/.*(GP[0-9]*).*/\1/p') == GP[0-9]* ]] && nv_arch=Pascal
+        [[ $(lspci | grep VGA | sed -rn 's/.*(GV[0-9]*).*/\1/p') == GV[0-9]* ]] && nv_arch=Volta
+        [[ $(lspci | grep VGA | sed -rn 's/.*(TU[0-9]*).*/\1/p') == TU[0-9]* ]] && nv_arch=Turing
+        [[ $(lspci | grep VGA | sed -rn 's/.*(GA[0-9]*).*/\1/p') == GA[0-9]* ]] && nv_arch=Ampere
+        [[ $(lspci | grep VGA | sed -rn 's/.*(AD[0-9]*).*/\1/p') == AD[0-9]* ]] && nv_arch=Ada_Lovelace
+    fi
+}
+
+nvidia_check_arch
+
+f [[ -n "$nv_arch" ]]; then
+    if [[ $nv_arch == Maxwell || $nv_arch == Pascal || $nv_arch == Volta || $nv_arch == Turing || $nv_arch == Ampere || $nv_arch == Ada_Lovelace ]]; then
+          export __GL_MaxFramesAllowed=3
+          export __GL_YIELD="USLEEP"
+          export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
+          export VDPAU_DRIVER=nvidia
+          export LIBVA_DRIVER_NAME=nvidia
+          export NVD_BACKEND=direct
+          export MOZ_DISABLE_RDD_SANDBOX=1
+    elif [[ $nv_arch == Kepler ]]; then
+            export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
+    else
+        # Nouveau
+        export VDPAU_DRIVER=nouveau
+        export LIBVA_DRIVER_NAME=nouveau
+    fi
+fi
+
 if [ -d /sys/module/nvidia ]; then
-  export __GL_THREADED_OPTIMIZATIONS=1
+  #General Tweaks
   export __GL_MaxFramesAllowed=3
   export __GL_YIELD="USLEEP"
   export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
+  #Bypass kernel panic
   sysctl -w vm.panic_on_oom=0
 fi
+
+
